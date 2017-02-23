37b9ba99a36d4158e6b216ad4fd8f2fd7ac6109d
sumantk <kulkarni.sumant@gmail.com>
2017-02-23 11:40:24 +0530

    Adding code to sample.py to test probability of a char sequence

--- sample.py
+++ sample.py
@@ -24,21 +24,54 @@ def main():
                        help='0 to use max at each timestep, 1 to sample at each timestep, 2 to sample on spaces')
 
     args = parser.parse_args()
+    print("Running function: sample" )
     sample(args)
+    print("\nRunning function: test_log_prob" )
+    test_log_prob(args)
 
 def sample(args):
+    # batch_size=1, data_dir='data/tinyshakespeare', decay_rate=0.97, grad_clip=5.0, init_from=None,
+    # learning_rate=0.002, model='lstm', num_epochs=50, num_layers=2, rnn_size=128, save_dir='save', save_every=1000,
+    # seq_length=1, vocab_size=65)
+
     with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
         saved_args = cPickle.load(f)
     with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'rb') as f:
         chars, vocab = cPickle.load(f)
+
     model = Model(saved_args, True)
+    print(model.probs)
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
         saver = tf.train.Saver(tf.global_variables())
         ckpt = tf.train.get_checkpoint_state(args.save_dir)
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
-            print(model.sample(sess, chars, vocab, args.n, args.prime, args.sample))
+            print(model.sample(sess, chars, vocab, args.n, args.prime, args.sample)), saved_args
+
+def test_log_prob(args):
+    # batch_size=1, data_dir='data/tinyshakespeare', decay_rate=0.97, grad_clip=5.0, init_from=None,
+    # learning_rate=0.002, model='lstm', num_epochs=50, num_layers=2, rnn_size=128, save_dir='save', save_every=1000,
+    # seq_length=1, vocab_size=65)
+
+    with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
+        saved_args = cPickle.load(f)
+    with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'rb') as f:
+        chars, vocab = cPickle.load(f)
+
+    print("Getting models using saved arguments: " + repr(saved_args))
+    model = Model(saved_args, True)
+    print(model.probs)
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        saver = tf.train.Saver(tf.global_variables())
+
+        ckpt = tf.train.get_checkpoint_state(args.save_dir)
+
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            log_probability = model.log_probability(sess, vocab, "abdcbd")
+            print(log_probability)
 
 if __name__ == '__main__':
     main()
